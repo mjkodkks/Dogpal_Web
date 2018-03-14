1364023be2b7081274547ab4bfab30be1ebf255c
DKKs <eye-q-21@hotmail.com>
2018-03-15 02:17:58 +0700

    test picture in box

--- src/uploadpic.js
+++ src/uploadpic.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Dropzone from 'react-dropzone';
 
 class Uploadpic extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imageFiles: []
+    }
+  }
+
+
   render() {
     return (
       <div className="uploadpic">
-        <div className="outside-box">
-        <div className="uploadpic-box">uploader</div>
+        <div className="outside-box" for="uploadfor">
+          <div className="uploadpic-box" ></div>
         </div>
+        <button className="btn"></button>
       </div>
     );
   }
